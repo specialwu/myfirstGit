8ed9af4100ce87b0722e5ccf65817053ab466843
specialwu <73089916+specialwu@users.noreply.github.com>
2020-10-22 15:04:01 +0800

    再次测试

---
+++ src/main/java/cdh005/Test.java
@@ -0,0 +1,13 @@
+package cdh005;
+
+/**
+ * @Author:wuqi
+ * @DATE: 2020/10/22 0022
+ * @TIME: 15:02
+ * @PROJECT_NAME: myfirstGit
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("成功");
+    }
+}
